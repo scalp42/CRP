d007f1dc37d69f5c4c2c60296fa798008163fc72
Sergii Golovatiuk <holser@gmail.com>
2013-04-24 09:38:45 +0300

    Fixing DB names

--- cookbooks/fuse_database/attributes/default.rb
+++ cookbooks/fuse_database/attributes/default.rb
@@ -1,3 +1,3 @@
-default['fuse_database']['name'] = 'vagrant_crp'
-default['fuse_database']['user'] = 'vagrant_crp'
+default['fuse_database']['name'] = 'vg_crp'
+default['fuse_database']['user'] = 'vg_crp'
 default['fuse_database']['password'] = 'super_secret'
--- cookbooks/tomcat_database/attributes/default.rb
+++ cookbooks/tomcat_database/attributes/default.rb
@@ -1,3 +1,3 @@
-default['tomcat_database']['name'] = "vagrant_crp_location"
-default['tomcat_database']['user'] = "vagrant_crp_location"
+default['tomcat_database']['name'] = "vg_crp_location"
+default['tomcat_database']['user'] = "vg_crp_location"
 default['tomcat_database']['password'] = "super_secret"
