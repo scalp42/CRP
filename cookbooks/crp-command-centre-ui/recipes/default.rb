476017e24e37729f5d20317a1de41ae62cbb72f7
Sergii Golovatiuk <holser@gmail.com>
2013-06-04 12:08:19 +0300

    Fixed Berksfile and version constraints

--- cookbooks/crp-command-centre-ui/recipes/default.rb
+++ cookbooks/crp-command-centre-ui/recipes/default.rb
@@ -10,12 +10,14 @@
 include_recipe "nginx"
 
 unless Chef::Config[:solo]
-include_recipe "ssl"
-template "#{node['nginx']['dir']}/sites-available/crp-command-centre-ui" do
-    source "crp-command-centre-ui.erb"
-    mode "0644"
-    nginx_site "crp-command-centre-ui" do
-      enable true
-    end
-end
+	include_recipe "ssl"
+	template "#{node['nginx']['dir']}/sites-available/crp-command-centre-ui" do
+	    source "crp-command-centre-ui.erb"
+	    mode "0644"
+	    notifies :reload, "service[nginx]"
+	end
+
+	nginx_site "crp-command-centre-ui" do
+	  enable true
+	end
 end
