7f67d9f40ca19b70a06d186461c63caf90e11bda
Sergii Golovatiuk <holser@gmail.com>
2013-06-13 20:22:03 +0300

    added cookbook for nginx-repo

---
+++ cookbooks/nginx-repo/metadata.rb
@@ -0,0 +1,9 @@
+name             'nginx-repo'
+maintainer       'EPAM Systems'
+maintainer_email 'Sergii Golovatiuk'
+license          'All rights reserved'
+description      'Installs/Configures nginx-repo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
